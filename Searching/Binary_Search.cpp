eb81e77a4ab04bab4be349d0d27d6ee8a1cf8a80
udiptagogoi185 <udiptagogoi185@gmail.com>
2024-03-13 20:12:59 +0530

    adding Binary Search

---
+++ Searching/Binary_Search.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int binary(int* arr, int low, int high, int element){
+    int mid;
+    while(low <= high){
+        mid = low + (high - low)/2;
+        if(arr[mid] == element){
+            return mid;
+        }
+        if(arr[mid] < element){
+            
+            low = mid+1;
+        }else{
+
+            high = mid;
+        }
+    }
+    return -1;
+}
+int main(){
+    int A[] = {3, 5, 7, 8, 9, 99};
+    int s = sizeof(A)/sizeof(A[0]);
+    int element = 7;
+    int search = binary(A, 0, s-1, element);
+    if(search == -1){
+        
+        cout << "not found";
+    }
+    cout << "found at index : " << search ;
+
+    return 0;
+}
